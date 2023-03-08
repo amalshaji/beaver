984b2ef57bcbc1ee39a288399e485a544b87c223
Amal Shaji <amalshajid@gmail.com>
2023-03-08 23:26:16 +0530

    API endpoint to create superuser

---
+++ internal/server/web/src/pages/CreateSuperUser.svelte
@@ -0,0 +1,149 @@
+<script lang="ts">
+  import { navigate } from "svelte-routing";
+  import beaverPng from "../assets/beaver.png";
+  import Loader from "../lib/Loader.svelte";
+
+  import toast from "svelte-french-toast";
+
+  let loading: boolean = false;
+
+  let email: string = "",
+    password: string = "";
+
+  const createSuperUser = async () => {
+    loading = true;
+    try {
+      const res = await fetch("/api/v1/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (res.status == 200) {
+        toast.success("Superuser created. Login to continue.");
+        const data = await res.json();
+        console.log(data);
+        navigate("/", { replace: true });
+      } else {
+        const data = await res.json();
+        toast.error(data.error);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      loading = false;
+    }
+  };
+</script>
+
+<div class="grid h-screen place-items-center w-full mx-auto">
+  <div
+    class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 w-full"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img class="mx-auto h-16 w-auto" src={beaverPng} alt="Beaver" />
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 border shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" on:submit|preventDefault={createSuperUser}>
+          <div>
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              Email address
+            </label>
+            <div class="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                bind:value={email}
+                required
+                class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              for="password"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div class="mt-1">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                bind:value={password}
+                minlength="6"
+                autocomplete="current-password"
+                required
+                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="w-full h-12 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              {#if loading}
+                <div class="my-auto">
+                  <Loader />
+                </div>
+              {:else}
+                <span class="my-auto">Create Superuser</span>
+              {/if}
+            </button>
+          </div>
+        </form>
+
+        <div class="mt-6">
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+              <div class="w-full border-t border-gray-300" />
+            </div>
+            <div class="relative flex justify-center text-sm">
+              <span class="px-2 bg-white text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div class="mt-6 grid grid-cols-3 gap-3">
+            <div />
+            <div>
+              <a
+                href="#"
+                aria-disabled="true"
+                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span class="sr-only">Sign in with Google</span>
+                <svg
+                  fill="#000000"
+                  class="h-5 w-5"
+                  version="1.1"
+                  id="Capa_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 210 210"
+                  xml:space="preserve"
+                >
+                  <path
+                    d="M0,105C0,47.103,47.103,0,105,0c23.383,0,45.515,7.523,64.004,21.756l-24.4,31.696C133.172,44.652,119.477,40,105,40
+	c-35.841,0-65,29.159-65,65s29.159,65,65,65c28.867,0,53.398-18.913,61.852-45H105V85h105v20c0,57.897-47.103,105-105,105
+	S0,162.897,0,105z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
